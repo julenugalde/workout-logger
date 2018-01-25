5cef21a452055ab15fac9ea72e99713690720114
Julen Ugalde <julen.ugalde@gmail.com>
2018-01-25 11:56:18 -0500

    Improvements to main and new session activities. Edit text with counter created. Translations.

---
+++ app/src/main/java/eus/julenugalde/workoutlogger/view/EditTextWithCounter.java
@@ -0,0 +1,66 @@
+package eus.julenugalde.workoutlogger.view;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+import eus.julenugalde.workoutlogger.R;
+import eus.julenugalde.workoutlogger.model.TrainingSession;
+import eus.julenugalde.workoutlogger.model.Workout;
+
+/** Edit text that incorporates a character counter in order to avoid surpassing the
+ * maximum length.
+ */
+@SuppressLint("AppCompatCustomView")
+public class EditTextWithCounter extends EditText {
+    /** Maximum text length allowed by the counter*/
+    public static final int COUNT_LIMIT = TrainingSession.MAX_LENGTH_COMMENT;
+    /** Text size in pixel units */
+    public static final float TEXT_SIZE = 30;
+
+    private Paint paintBackground;
+    private Paint paintForeground;
+    private float scale;
+
+    public EditTextWithCounter(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initialize();
+    }
+
+    public EditTextWithCounter(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initialize();
+    }
+
+    public EditTextWithCounter(Context context) {
+        super(context);
+        initialize();
+    }
+
+    private void initialize() {
+        paintBackground = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paintBackground.setColor(getResources().getColor(R.color.colorPrimaryDark));
+        paintBackground.setStyle(Paint.Style.FILL);
+
+        paintForeground = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paintForeground.setColor(getResources().getColor(R.color.colorTextIcons));
+        paintForeground.setTextSize(TEXT_SIZE);
+
+        scale = getResources().getDisplayMetrics().density;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        //Paint the background with dark color
+        canvas.drawRect(this.getWidth()-30*scale, 5*scale, this.getWidth()-(5*scale),
+                20*scale, paintBackground);
+        //Paint the number of characters
+        canvas.drawText("" + (COUNT_LIMIT-this.getText().toString().length()),
+                this.getWidth()-28*scale, 17*scale, paintForeground);
+    }
+}
--- app/src/main/java/eus/julenugalde/workoutlogger/view/MainActivity.java
+++ app/src/main/java/eus/julenugalde/workoutlogger/view/MainActivity.java
@@ -1,11 +1,15 @@
 package eus.julenugalde.workoutlogger.view;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.format.DateFormat;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
@@ -17,6 +21,7 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Locale;
 
 import eus.julenugalde.workoutlogger.R;
 import eus.julenugalde.workoutlogger.model.TrainingSession;
@@ -51,7 +56,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO open activity to record new training session
+                Intent intent = new Intent(MainActivity.this, ActivityNewTrainingSession.class);
+                startActivityForResult(intent, REQ_CODE_ADD_TRAINING_SESSION);
             }
         });
 
@@ -68,7 +74,6 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-
         workoutData = new WorkoutData(getApplicationContext());
         updateTrainingSessionList();
     }
@@ -110,17 +115,70 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        //TODO Include menu?
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        Intent intent;
+        switch(item.getItemId()) {
+            case R.id.main_activity_menu_list_workouts:
+                //TODO list of workouts
+                return true;
+            case R.id.main_activity_menu_load_xml:
+                //TODO load xml
+                return true;
+            case R.id.main_activity_menu_save_xml:
+                //TODO save xml
+                return true;
+            case R.id.main_activity_menu_settings:
+                //TODO settings windows
+                return true;
+            case R.id.main_activity_menu_view_graph:
+                //TODO view graph
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQ_CODE_ADD_TRAINING_SESSION:
+                switch (resultCode) {
+                    case RESULT_OK:
+                        Toast.makeText(getApplicationContext(),
+                                R.string.main_activity_save_session_ok, Toast.LENGTH_LONG).show();
+                        updateTrainingSessionList();
+                        break;
+                    case RESULT_ERROR_SAVE:
+                        Toast.makeText(getApplicationContext(),
+                                R.string.main_activity_save_session_error, Toast.LENGTH_LONG).show();
+                        break;
+                }
+            case REQ_CODE_VIEW_TRAINING_SESSION:
+            case REQ_CODE_VIEW_WORKOUT:
+                updateTrainingSessionList();
+                break;
+            case REQ_CODE_SETTINGS:
+                updateLocale();
+                break;
+            default:
+                Log.e(TAG, "Error: invalid requestCode value");
+                break;
+        }
+    }
+
+    private void updateLocale() {
+        //Access preferences
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+        String languageCode = prefs.getString("selectedLocale", "en");
+
+        //Update locale
+        Locale myLocale = new Locale(languageCode);
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        Configuration configuration = getResources().getConfiguration();
+        configuration.setLocale(myLocale);
+        getResources().updateConfiguration(configuration, displayMetrics);
     }
 }
